0d6a8308cb4f0fce25dd501ef5b8e5a218f59dd3
Behruz <behruz.berdiyev.0129@gmail.com>
2022-03-06 12:59:12 +0500

    Pages and rounting

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+export default function AboutPage() {
+	return (
+		<div>
+			<h1>Biz Haqimizda</h1>
+			<p>
+				Bu dastur orqali siz Xovos tumanida bo`ladigan va bo`lishi kutilayotgan
+				mahalliy tanlovlar, viktorinalar va o`yinlar haqida ma`lumot olishingiz
+				mumkin.
+			</p>
+			<p>Versiya: 1.0.0</p>
+		</div>
+	);
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+	return (
+		<div>
+			<h1>My Events 1</h1>
+		</div>
+	);
+}
